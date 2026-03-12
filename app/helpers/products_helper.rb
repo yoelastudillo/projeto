364ca94672bdf58873d8500cb8663c2ad0c33f29
yoelastudillo <yoel.reich@sefin.fortaleza.ce.gov.br>
2026-03-12 12:02:04 -0300

    actualizacion diaria

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,5 @@
 module ProductsHelper
+  def month_day_year(datetime)
+    datetime.strftime("%B %e, %Y")
+  end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,4 +2,11 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "bootstrap"
+import "@popperjs/core"
 
+import * as bootstrap from "bootstrap"
+
+document.addEventListener("turbo:load", () => {
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    .forEach(el => new bootstrap.Tooltip(el))
+})
